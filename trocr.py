406a2b43f0a669bec41b73f33b4b313a74672ad5
cypx <cyprien.d@gmail.com>
2013-03-15 10:12:55 +0100

    reverse order of items into gallery

--- trocr.py
+++ trocr.py
@@ -93,7 +93,7 @@ def show_entries():
 		cur = g.db.execute('SELECT title, date, descr, filename, size, mime, gallery_id FROM entries WHERE filename like "{pid}.%" ORDER BY id desc;'.format(
 			pid=requested_id))
 	elif (requested_gallery != ''):
-		cur = g.db.execute('SELECT title, date, descr, filename, size, mime, gallery_id FROM entries WHERE gallery_id="{gid}" ORDER BY id desc;'.format(
+		cur = g.db.execute('SELECT title, date, descr, filename, size, mime, gallery_id FROM entries WHERE gallery_id="{gid}" ORDER BY id asc;'.format(
 			gid=requested_gallery))
 	else:
 		cur = g.db.cursor()
@@ -253,7 +253,7 @@ def edit_entry():
 			cur = g.db.execute('SELECT title, date, descr, filename, size, mime, gallery_id FROM entries WHERE filename like "{pid}.%" ORDER BY id desc;'.format(
 				pid=requested_id))
 		elif (requested_gallery != ''):
-			cur = g.db.execute('SELECT title, date, descr, filename, size, mime, gallery_id FROM entries WHERE gallery_id="{gid}" ORDER BY id desc;'.format(
+			cur = g.db.execute('SELECT title, date, descr, filename, size, mime, gallery_id FROM entries WHERE gallery_id="{gid}" ORDER BY id asc;'.format(
 				gid=requested_gallery))
 		entries = [dict(title=row[0], date=time.strftime("%D %H:%M", time.localtime(int(row[1]))), id=row[3].rsplit('.', 1)[0], descr=row[2], filename=row[3], size=sizeof_fmt(row[4]), mime=row[5], type=row[5].split("/")[0]) for row in cur.fetchall()]
 		return render_template('edit_entries.html', entries=entries)
